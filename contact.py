c9661636fa63f8e8bd387cb5a82dff9dbd8e6bca
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-05-12 17:40:00 +0200

    guardando datos cliente

---
+++ contact.py
@@ -0,0 +1,29 @@
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+db = SQLAlchemy()
+
+class Contacto(db.Model):
+    id = db.Column(db.String(100))
+    nombre = db.Column(db.String(100))
+    correo = db.Column(db.String(100))
+    telefono = db.column(db.String(100))
+    
+    def __init__(self,nombre, correo, telefono):
+        self.nombre = nombre
+        self.correo = correo
+        self.telefono = telefono
+    
+    
+    
+    
+    
+        
+contactos = Blueprint("contactos", __name__)
+
+
+@contactos.route('/')
+def index():
+    contactos = 
