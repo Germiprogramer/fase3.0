09478a05bc8f718a3087ba999b2b3ae8e5be37e7
rnoguer22 <rnogugon@myuax.com>
2022-05-12 13:35:48 +0200

    metodo grafico simple

--- Graficos/grafico.py
+++ Graficos/grafico.py
@@ -6,6 +6,17 @@ class Grafico:
     def __init__(self, archivo):
         self.archivo = archivo
         self.dt = pd.read_csv(self.archivo)
+
+    def grafico_simple(self,tipo_grafico, columna):
+
+        fig, ax = plt.subplots()
+
+        self.dt[columna].sum().plot(kind=tipo_grafico, ax=ax)
+
+        ax.set_title("Grafico {}".format(columna), loc = "center", fontdict = {'fontsize':14, 
+        'fontweight':'bold', 'color':'tab:blue'})
+        ax.set_ylabel('')
+        plt.savefig('img/grafico' + ' - '+ columna + '.png', bbox_inches='tight')
     
     def grafico(self,tipo_grafico,col1,col2):
 
@@ -16,20 +27,7 @@ class Grafico:
         ax.set_title("Grafico {} {}".format(col1, col2), loc = "center", fontdict = {'fontsize':14, 
         'fontweight':'bold', 'color':'tab:blue'})
         ax.set_ylabel('')
-        plt.savefig('Graficos/grafico' + ' - '+ col1 + col2 + '.png', bbox_inches='tight')
+        plt.savefig('img/grafico' + ' - '+ col1 + col2 + '.png', bbox_inches='tight')
 
-
-#Una segunda forma de crear graficos
-
-def grafico_2(dataset, tipo_grafico, titulografico, nombregrafico):
-    fig, ax = plt.subplots()
- 
-    dataset.plot(kind=tipo_grafico, ax = ax, color = "red")
-    ax.set_title(str(titulografico), loc = "center", fontdict = {'fontsize':14, 'fontweight':'bold', 'color':'tab:orange'})
-    ax.set_ylabel('')
-
-    plt.savefig('{}.png'.format(nombregrafico), bbox_inches='tight')
-
-    plt.show()
-
-grafico_2(calidad, "hist", "CALIDAD VINO", "grafico")
+grafico1 = Grafico("WineQT.csv")
+grafico1.grafico("bar","")
